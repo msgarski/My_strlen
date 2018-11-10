89ae804dcf04c8e4b1923c5ccda220c917567568
Robert Gorski <garski@gmail.com>
2018-11-10 09:22:33 +0100

    pierrwszy commit calkowicie z konsoli

---
+++ Mystrlen.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	char a[]={"123456789a"};
+	int len=0;
+	
+	while(a[len])
+	{
+		len++;
+	}
+	
+	
+	
+	
+	
+	std::cout<<"dlugosc tablicy a ="<<len<<std::endl;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
